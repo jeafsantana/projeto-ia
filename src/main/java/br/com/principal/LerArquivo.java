c6e777e8a50823051385051101591f88fc88d027
Jessica Santana <jeafsantana@hotmail.com>
2017-11-25 01:32:42 -0200

    textos

--- src/main/java/br/com/principal/LerArquivo.java
+++ src/main/java/br/com/principal/LerArquivo.java
@@ -1,7 +1,8 @@
 package br.com.principal;
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class LerArquivo {
@@ -9,10 +10,12 @@ public class LerArquivo {
 	public String LerArquivoDeEsportes() {
 		StringBuffer texto = new StringBuffer();
 		try {
-			BufferedReader info = new BufferedReader(new FileReader("texto/teste.txt"));
+			BufferedReader info = new BufferedReader(new FileReader("texto/texto1-esportes.txt"));
 			// TODO: Tratar o arquivo de texto antes de retornar!
 			String linha = info.readLine();
+			String[] palavra;
 			while (linha != null) {
+				linha = tratarTexto(linha);
 				texto.append(linha);
 				linha = info.readLine();
 			}
@@ -21,5 +24,29 @@ public class LerArquivo {
 		}
 		return texto.toString();
 	}
+	
+	public String tratarTexto(String linha) {
+		
+		linha=linha.replace("<head>", " ");
+		linha=linha.replace("<edic>", " ");
+		linha=linha.replace("</edic>", " ");
+		linha=linha.replace("<autor>", " ");
+		linha=linha.replace("</autor>", " ");
+		linha=linha.replace("</head>", " ");
+		linha=linha.replace("<body>", " ");
+		linha=linha.replace("<subtítulo>", " ");
+		linha=linha.replace("</subtítulo>", " ");
+		linha=linha.replace("<título>", " ");
+		linha=linha.replace("</título>", " ");
+		linha=linha.replace("</body>", " ");
+		linha=linha.replace("<", " ");
+		linha=linha.replace("|", " ");
+		linha=linha.replace("/", " ");
+		
+		
+		
+		return linha;
 
+	}
+	
 }
